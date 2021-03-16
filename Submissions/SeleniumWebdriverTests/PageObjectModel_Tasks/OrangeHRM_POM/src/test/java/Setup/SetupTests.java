2bc30c10a07b39a931876c92b4e4050d0f84f645
Maureen-perez <maureenupm@gmail.com>
2021-03-16 16:23:51 -0700

    SeleniumWebdriverTests

---
+++ Submissions/SeleniumWebdriverTests/PageObjectModel_Tasks/OrangeHRM_POM/src/test/java/Setup/SetupTests.java
@@ -0,0 +1,51 @@
+package Setup;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import pageObject.DashboardPage;
+import pageObject.LoginPage;
+
+public class SetupTests {
+
+    //Create a WebDriver Object
+    private WebDriver driver;
+
+    //Create LoginPage object
+    protected LoginPage loginPage;
+
+    //Create DashboardPage object
+    protected DashboardPage dashboardPage;
+    @BeforeTest
+    public void setUp()
+    {
+        //Selenium WebDriver needs this executable in order to know which browser to run your test
+        //Browser setup for Chrome
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        //Instantiate WebDriver object
+        driver = new ChromeDriver();
+
+        /* Browser setup for Firefox
+        System.setProperty("webdriver.gecko.driver", "E:/test_automation/resources/geckodriver.exe");
+        driver = new FirefoxDriver();
+         */
+
+        //Any activity you want to perform concerning your automated test can now be done using the driver
+
+        //Maximize window size
+        driver.manage().window().maximize();
+
+        //Launch the application
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+
+        //Instantiate login page after launching the browser - Handle
+        loginPage = new LoginPage(driver);
+    }
+    @AfterTest
+    public void closeBrowser()
+    {
+        // ".close" would close the window but the ".quit" would exit the session
+        driver.quit();
+    }
+}
